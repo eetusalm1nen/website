90906f051952a4c67b9e2d51d56da73acb5aa345
Eetu Salminen <152289405+eetusalm1nen@users.noreply.github.com>
2025-12-18 14:46:41 +0200

    Refactor Raspberry Pi section and update descriptions

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -34,28 +34,30 @@ function Projects() {
             <li><small>Simple dynamic web page for personal use. Code on GitHub .</small></li>  
           </ul>
         
-          <h3>Raspberry Pi</h3>
-          <ul className="irl-list">
-            <li><small>Hosting my website from a Raspberry Pi server.</small></li>
-            <li><small>Smart mirror using MagicMirror.</small></li>
-            <li><small>Custom webhooks for automated notifications. Code on GitHub.</small></li>
-            <li><small>Adblocker using Pi-hole.</small></li>
-          </ul>
-         
       
           <h3>School projects</h3>
           <ul className="irl-list">
-            <li><small>Few of the most meaningful projects on GitHub.</small></li>
+            <li><small>Few of the most meaningful coding projects on GitHub.</small></li>
+            <li><small>Bachelor's Thesis: Utilizing Modern Mobile Network Technologies in Production Line Digital Twins (In Finnish).</small></li>
           </ul>
          
 
           <h3>Other</h3>
           <ul className="irl-list">
-            <li><small>Co-founder of Root.</small></li>
+            <li><small>Co-founder & Head of Partnerships of Root.</small></li>
             <a href="https://rootexpo.fi" className="red-question">Root Expo</a>
             <li><small>Boost Turku | Board '26.</small></li>
             <a href="https://www.boostturku.com/" className="red-question">Boost Turku</a>
           </ul>
+
+      
+          <h3>Raspberry Pi</h3>
+          <ul className="irl-list">
+            <li><small>Smart mirror using MagicMirror.</small></li>
+            <li><small>Custom webhooks for automated notifications. Code on GitHub.</small></li>
+            <li><small>Adblocker using Pi-hole.</small></li>
+          </ul>
+         
 
           <br />
 
@@ -67,10 +69,13 @@ function Projects() {
           <br />
           <br />
 
+      /*
           <span>Psst.. backend is feeling kinda useless rn. By pressing this button you can make it feel important: </span> 
             <button className="click-button" onClick={handleClick}>  
             {clickCount}
             </button>
+      */
+      
       </section>
     )
 }
